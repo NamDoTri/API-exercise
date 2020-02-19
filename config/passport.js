c155d14ab3472e616349fa8efeac14f44b355afa
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2020-02-19 23:27:06 +0200

    Configured passport and installed other necessary libraries for auth

---
+++ config/passport.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const ppJwt = require('passport-jwt');
+const Strategy = ppJwt.Strategy;
+const ExtractJwt = ppJwt.ExtractJwt;
+const secret = process.enc.HASH_SECRET || "secret";
+const mongoose = require('mongoose');
+const User = require('../models/user');
+
+const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: secret
+};
+
+module.exports = (passport) => {
+    passport.use(new Strategy(options, (payload, done) => {
+        User.findById(payload.id)
+            .then(user => {
+
+                if(user){
+                    return done(null, {id: user.name, email: user.email});
+                }
+
+                return done(null, false);
+            })
+            .catch(err => console.log(err));
+    }));
+};
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const app = require('express')();
 const port = process.env.PORT || 3008;
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fs = require('fs');
+const cp = require('cookie-parser');
 const mongoose = require('mongoose');
-require('dotenv').config();
+const passport = require('passport');
+const passconfig = require("./config/passport");
+
 
 const setup = async () => {
     try{
@@ -14,17 +17,27 @@ const setup = async () => {
             useUnifiedTopology: true
         });
 
-
+        //middlewares
+        app.use(passport.initialize());
+        passconfig(passport);
+        app.use(cp());
+        app.use(bodyParser.urlencoded({extended: false}));
         app.use(bodyParser.json());
         app.use(cors());
+        app.use((req,res,next) => {
+            if (req.body) log.info(req.body);
+            if (req.params) log.info(req.params);
+            if(req.query) log.info(req.query);
+            log.info(`Received a ${req.method} request from ${req.ip} for ${req.url}`);
+            next();
+        });
+
+
         const ItemRoute = require('./routes/item');
-
-
         app.use('/items', ItemRoute);
 
-        app.get('/', (req, res) => {
-            res.send("Hello!")
-        });
+        const UserRoute = require("./routes/user");
+        app.use('/users', UserRoute);
 
         app.listen(port, () => {
             console.log(`Listening on port ${port}...`)
